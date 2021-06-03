1c01a1801f9483be35b32814cb514dccba67df2c
Prachi Bile <bileprachi@gmail.com>
2021-06-03 11:02:36 +0530

    Common class for pop-ups

---
+++ PhotoGalleryApp/Utility/PopUp.swift
@@ -0,0 +1,14 @@
+//
+//  PopUp.swift
+//  PhotoGalleryApp
+//
+//  Created by Prachi Bile on 02/06/21.
+//
+
+import Foundation
+import UIKit
+
+class PopUp: UIAlertController {
+    let alert = UIAlertController(title: "Error Encountered", message: "Something wrong with API or check your internet connection", preferredStyle: .alert)
+//    let action = UIActio
+}

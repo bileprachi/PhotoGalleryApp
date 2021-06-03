48fee6035ea38f98909926d8e96bf95ef56731c2
Prachi Bile <bileprachi@gmail.com>
2021-06-03 11:01:07 +0530

    Added for networkError message on VC

---
+++ PhotoGalleryApp/Controller/InternetConnectionMessageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InternetConnectionMessageViewController.swift
+//  PhotoGalleryApp
+//
+//  Created by Prachi Bile on 03/06/21.
+//
+
+import UIKit
+
+class InternetConnectionMessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}

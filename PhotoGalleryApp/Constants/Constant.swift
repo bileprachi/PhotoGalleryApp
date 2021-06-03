b66b1999fda4dd7e38e3a05f02e08cf64131a797
Prachi Bile <bileprachi@gmail.com>
2021-06-03 11:03:03 +0530

    Added Error handling

--- PhotoGalleryApp/Constants/Constant.swift
+++ PhotoGalleryApp/Constants/Constant.swift
@@ -8,8 +8,24 @@
 import Foundation
 import UIKit
 
-enum Constant {
+enum GalleryURL {
     static let galleryURL = "https://www.flickr.com/services/feeds/photos_public.gne?tags=soccer&format=json&nojsoncallback=?"
+}
+
+enum GalleryPlaceholderImage {
     static let placeholderCatImage = UIImage(named: "PlaceholderCatImage")
+}
+
+enum GalleryStoryboardID {
     static let imageStoryboardID = "ImageVC"
+    static let internetConnectStoryboardID = "InternectConnectionMessageVC"
+    static let errorStoryboardID = "ErrorVC"
+}
+
+enum GalleryError: String {
+    case noError = ""
+    case decodeError = "Error encountered while decoding data..."
+    case responseError = "Error received from response..."
+    case noDataError = "No Data received from response..."
+    case invalidStatusError = "API called failed,status code is not equal to 200..."
 }

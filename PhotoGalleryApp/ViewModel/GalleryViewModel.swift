7e978afa4b4503bfa856c1cbd39190f56e1a9673
Prachi Bile <bileprachi@gmail.com>
2021-06-03 10:59:39 +0530

    Added Error handling

--- PhotoGalleryApp/ViewModel/GalleryViewModel.swift
+++ PhotoGalleryApp/ViewModel/GalleryViewModel.swift
@@ -10,22 +10,27 @@ import Foundation
 protocol GalleryViewModelDelegate: AnyObject {
     func didGetNoOfRowsInSection(rows count: Int)
     func didGetGalleryData(galleryData gallery: GalleryData)
+    func didFailWithError(responseErr responseError: String)
 }
 
 class GalleryViewModel {
     
     weak var delegate: GalleryViewModelDelegate?
     
-    let galleryUrl = Constant.galleryURL
+    let galleryUrl = GalleryURL.galleryURL
     var galleryManagerObj = GalleryManager()
     
     func fetchGalleryData() {
         print("Inside fetchGalleryData...GalleryUrl: \(galleryUrl)")
         galleryManagerObj.getGalleryData(for: galleryUrl, completionHandler: {
-            galleryData in
-            
+            galleryData,error  in
+            guard error == GalleryError.noError.rawValue else {
+                self.delegate?.didFailWithError(responseErr: error)
+                return
+            }
             self.delegate?.didGetNoOfRowsInSection(rows: (galleryData?.items.count)!)
             self.delegate?.didGetGalleryData(galleryData: galleryData!)
+            self.delegate?.didFailWithError(responseErr: error)
             
         })
     }
